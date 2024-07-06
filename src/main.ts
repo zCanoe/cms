0b78ccbf176275f905df903b35b9bd99e3017e01
canoe <760548276@qq.com>
2024-07-06 16:10:41 +0800

    高阶组件

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,6 @@ import { createApp } from "vue";
 import router from "./router";
 import pinia from "./store";
 import registerIcons from "@/global/register-icons";
-import { createPersistedState } from "pinia-plugin-persistedstate";
-// import "element-plus/theme-chalk/el-message.css";
-// import "element-plus/dist/index.css";
 
 const app = createApp(App);
 
